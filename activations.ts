213474ca2d73a3e9307f1996999f5736e3270ab5
klimov.roman10 <timeflieswhenyouhf@internet.ru>
2024-09-14 17:09:41 +0300

    reimagine some implementation - introduce activations chart

---
+++ activations.ts
@@ -0,0 +1,20 @@
+// actionations chart is used to address establishments are to be played in round
+// after a dice toss
+
+// notice that these establishments are already ordered in right order supposed in
+// the rulebook
+
+const table = {
+  1: ["wheat field"],
+  2: ["livestock farm", "bakery"],
+  3: ["cafe", "bakery"],
+  4: ["conbini"],
+  5: ["forest"],
+  6: [], // all major establishments - for nor i guess there are predefined states ahead for each of them
+  7: ["cheese factory"],
+  8: ["furniture factory"],
+  9: ["restraunt", "mine"],
+  10: ["restraunt", "apple orchad"],
+  11: ["produce market"],
+  12: ["produce market"],
+};
