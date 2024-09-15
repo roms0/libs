9d254798b8218dcd4e306b3e09a5ae6d3ce30357
klimov.roman10 <timeflieswhenyouhf@internet.ru>
2024-09-15 15:14:00 +0300

    continue activations and establishment cards mechanics

--- activations.ts
+++ activations.ts
@@ -4,6 +4,38 @@
 // notice that these establishments are already ordered in right order supposed in
 // the rulebook
 
+const wheat_field = {
+  do(owns: number, turns: number) {
+    if (owns === turns) {
+      console.log("nothing");
+      return;
+    }
+    console.log(turns, " pays to ", owns);
+  },
+};
+const livestock_farm = {
+  do(owns: number, turns: number) {
+    console.log(owns, " takes");
+  },
+};
+const bakery = {
+  do(owns: number, turns: number) {
+    console.log(owns, " takes");
+  },
+};
+
+const lookup = {
+  "wheat field": wheat_field,
+  "livestock farm": livestock_farm,
+  bakery: bakery,
+};
+
+const counts = {
+  "wheat field": [3, 0, 2],
+  "livestock farm": [1, 1, 1],
+  bakery: [1, 1, 0],
+};
+
 const table = {
   1: ["wheat field"],
   2: ["livestock farm", "bakery"],
@@ -18,3 +50,12 @@ const table = {
   11: ["produce market"],
   12: ["produce market"],
 };
+
+const dice = 1;
+const turns = 0;
+const ests = table[dice];
+ests.forEach((est) => {
+  for (let i = 0; i < counts[est][turns]; i++) {
+    lookup[est].do(i, turns);
+  }
+});
