d3271baa2f71da42d3133d258ceb1154b58407f6
klimov.roman10 <timeflieswhenyouhf@internet.ru>
2024-09-25 22:35:21 +0300

    clear

--- state-idle.ts
+++
@@ -1,9 +0,0 @@
-import { HeroState } from "./model";
-
-export const state_idle: HeroState = {
-  title: "idle",
-  starts() {
-    console.log(this.title);
-  },
-  reacts() {},
-};
--- static-ordered-states.ts
+++
@@ -1,8 +0,0 @@
-import { start_pipeline } from "./pipeline-state-machine";
-import { state_idle } from "./state-idle";
-import { state_take_dice } from "./state-take-dices";
-import { state_toss_dice } from "./state-toss-dices";
-
-let i = 0;
-const states = [state_idle, state_take_dice, state_toss_dice];
-start_pipeline(states, i);
