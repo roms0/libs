862c1e4baf3c88436c878db137a34fd2b152d61c
klimov.roman10 <timeflieswhenyouhf@internet.ru>
2025-01-23 11:27:35 +0300

    continue making out intention logic

--- intention.ts
+++ intention.ts
@@ -6,26 +6,58 @@ class Utils {
 
 class State {
   utils = new Utils();
-  readonly intentions: Record<string, Intention> = {};
-  readonly items: Record<string, StateItem> = {};
-  add_intention(intention: Intention) {
-    this.intentions[intention.id] = intention;
+  log() {
+    console.log(this);
   }
+  get dice(): DiceItem | null {
+    if (this._dice) {
+      return this._dice;
+    }
+    const dice = Object.values(this.items).find((item) => isDiceItem(item));
+    if (!dice) {
+      return null;
+    }
+    this._dice = dice;
+    return dice;
+  }
+  private _dice: DiceItem;
+  public intentions: Record<string, Intention<any>> = {};
+  readonly items: Record<string, Item> = {};
+  readonly actors: Record<string, Actor> = {};
   add_item(item_title: keyof typeof items_titles) {
     const item = new items[item_title](this);
     this.items[item.id] = item;
   }
   generate_intentions() {
+    this.intentions = {};
     for (const id in this.items) {
       this.items[id].intention(this);
     }
   }
-  perform_action(payload: { iuid: string; data: unknown }) {
-    this.intentions[payload.iuid].perform(payload.data);
+  perform_action(payload: {
+    data: any;
+    meta: { actor_id: string; iuid: string };
+  }) {
+    this.intentions[payload.meta.iuid].perform({
+      data: payload.data,
+      meta: { actor_id: payload.meta.actor_id },
+    });
+  }
+  create_intention<T>(
+    title: string,
+    data: T,
+    perform: (args: { data: T; meta: { actor_id: string } }) => void
+  ) {
+    const id = this.utils.uid();
+    this.intentions[id] = { id, title, data, perform };
   }
 }
 
-class StateItem {
+class Actor {
+  networth: number;
+}
+
+class Item {
   id: string;
   title: string;
   constructor(state: State) {
@@ -34,88 +66,118 @@ class StateItem {
   intention(state: State) {}
 }
 
-function createIntention<T>(
-  state: State,
-  title: string,
-  data: T,
-  perform: (data: T) => void
-): Intention {
-  return {
-    id: state.utils.uid(),
-    title,
-    data,
-    perform,
-  };
-}
-
-interface Intention {
+interface Intention<T> {
   id: string;
   title: string;
-  data: unknown;
-  perform: (data) => void;
+  data: T;
+  perform: (args: { data: T; meta: { actor_id: string } }) => void;
 }
 
-class DiceItem extends StateItem {
+class DiceItem extends Item {
   title = "dice";
   points = 0;
   intention(state: State): void {
-    state.add_intention(
-      createIntention(state, items_titles.dice, { points: 1 }, ({ points }) => {
-        this.points = Math.trunc(Math.random() * 10);
-      })
+    state.create_intention(items_titles.dice, null, () => {
+      this.points = Math.trunc(Math.random() * 10);
+    });
+  }
+}
+
+function isDiceItem(item: Item) {
+  return item instanceof DiceItem;
+}
+
+class SushiBarItem extends Item {
+  title = "sushi_bar";
+  intention(state: State): void {
+    state.create_intention(
+      items_titles.sushi_bar,
+      {
+        dice_id: Object.values(state.items).find(
+          (item) => item.title === items_titles.dice
+        )?.id,
+      },
+      ({ data }) => {
+        if (!data.dice_id) return;
+        const item = state.items[data.dice_id];
+        if (isDiceItem(item)) {
+          item.points = 100;
+        }
+      }
     );
   }
 }
 
-function isDiceItem(item: StateItem) {
-  return "points" in item;
+class BusinessCenterEstablishment extends Item {
+  title = items_titles.business_center;
+  intention(state: State): void {
+    state.create_intention(
+      this.title,
+      { mine_id: state.items, his_id: state.items },
+      () => {
+        console.log("BUSINESS CENTER MECHANICS");
+      }
+    );
+  }
 }
 
-class SushiBarItem extends StateItem {
-  title = "sushi_bar";
+class TechStrartupEstablishment extends Item {
+  title = items_titles.tech_startup;
+  networth = 1;
   intention(state: State): void {
-    state.add_intention(
-      createIntention(
-        state,
-        items_titles.sushi_bar,
-        {
-          dice_id: Object.values(state.items).find(
-            (item) => item.title === items_titles.dice
-          )?.id,
-        },
-        ({ dice_id }) => {
-          console.log(dice_id);
-          if (!dice_id) return;
-          if (isDiceItem(state.items[dice_id])) {
-            state.items[dice_id].points = 100;
-          }
-        }
-      )
-    );
+    const dice = state.dice;
+    if (!dice) return;
+    if (dice.points !== 5) {
+      state.create_intention(this.title, null, () => {
+        console.log("INVEST");
+      });
+    }
+    if (dice.points === 5) {
+      state.create_intention(this.title, null, () => {
+        console.log(`GET REVENUE ${this.networth}`);
+      });
+    }
   }
 }
 
 const items_titles = {
   dice: "dice",
   sushi_bar: "sushi_bar",
+  business_center: "business_center",
+  tech_startup: "tech_startup",
 };
 
-const items: { [K in keyof typeof items_titles]: typeof StateItem } = {
+const items: { [K in keyof typeof items_titles]: typeof Item } = {
   dice: DiceItem,
   sushi_bar: SushiBarItem,
+  business_center: BusinessCenterEstablishment,
+  tech_startup: TechStrartupEstablishment,
 };
 
 // start
 const state = new State();
+state.add_item("tech_startup");
 state.add_item("dice");
-state.add_item("sushi_bar");
 state.generate_intentions();
-
-const intention = Object.values(state.intentions).find(
-  (intention) => intention.title === items_titles.sushi_bar
+const diceint = Object.values(state.intentions).find(
+  (intention) => intention.title === items_titles.dice
 );
-if (intention) {
-  state.perform_action({ iuid: intention?.id, data: intention?.data });
+if (diceint) {
+  state.perform_action({
+    meta: { iuid: diceint?.id, actor_id: "1" },
+    data: diceint?.data,
+  });
+}
+state.generate_intentions();
+const techint = Object.values(state.intentions).find(
+  (intention) => intention.title === items_titles.tech_startup
+);
+console.log(state.dice?.points);
+if (techint) {
+  state.perform_action({
+    meta: { iuid: techint?.id, actor_id: "1" },
+    data: techint?.data,
+  });
 }
 
 export {};
