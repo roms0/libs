61f8d692b9a6c1235441bfdad05bfc600135341d
klimov.roman10 <timeflieswhenyouhf@internet.ru>
2025-02-21 14:57:39 +0300

    common supplier

--- units/1.ts
+++ units/1.ts
@@ -1,4 +1,34 @@
-import { Data, isDices, isMaster, isTouched, points, State } from "./types";
+import {
+  Data,
+  Dices,
+  ID,
+  isDices,
+  isMaster,
+  isTouched,
+  Master,
+  points,
+  State,
+  Touch,
+} from "./types";
+
+export function read_supplier(state: State, data: Data) {
+  let supplier: null | (ID & Touch & Dices) = null;
+  const touched = Object.values(data).filter((item) => isTouched(item));
+  if (!isDices(touched[0])) return;
+  if (isMaster(touched[0]) && touched[0].master === state.turns(data).id) {
+    supplier = touched[0];
+  }
+  if (!isMaster(touched[0])) {
+    supplier = touched[0];
+  }
+  if (!supplier) return;
+  points(
+    supplier,
+    new Array(supplier.dices)
+      .fill(undefined)
+      .map(() => Math.trunc(Math.random() * 10))
+  );
+}
 
 export function read_points_supplier(state: State, data: Data) {
   const touched = Object.values(data).filter((item) => isTouched(item));
